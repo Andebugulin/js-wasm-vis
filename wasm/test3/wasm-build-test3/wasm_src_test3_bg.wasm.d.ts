27a45e932dd3ee09f304a84121cbbc463d7e9bc7
Andebugulin <gulin.andrey2005@gmail.com>
2025-11-28 12:22:49 +0200

    added files of wasm

---
+++ wasm/test3/wasm-build-test3/wasm_src_test3_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const edge_detection: (a: any) => [number, number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
